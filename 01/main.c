61534a80a50eec4950f7190982c215984b67d491
Drake <axylos@pop-os.localdomain>
2019-12-05 09:45:09 -0500

    init

---
+++ 01/main.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <assert.h>
+
+int fuel_helper(int mass) {
+  return (mass / 3) - 2;
+}
+
+int compute_fuel(int mass) {
+  int m = mass;
+  int sum = 0;
+
+  while (m > 0) {
+    m = fuel_helper(m);
+    if (m > 0) {
+      sum += m;
+    }
+  }
+
+  return sum;
+}
+
+void test() {
+  assert(0);
+}
+
+int main (int argc, char* argv[]) {
+
+  test();
+  char str[50];
+  int i;
+  int sum_mass = 0;
+  while (fscanf(stdin, "%d\n", &i) > 0) {
+    int mass = (i / 3) - 2;
+    sum_mass += mass;
+
+    int extra_fuel_mass = compute_fuel(mass);
+    sum_mass += extra_fuel_mass;
+  }
+
+  printf("The sum of the module masses is: %d\n", sum_mass);
+
+  return 0;
+}
