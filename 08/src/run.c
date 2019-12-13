8471d0c1adf4b4f8d3906b81ea0f3557a32aa8a6
axylos <robertdraketalley@gmail.com>
2019-12-12 20:47:06 -0500

    day 8 part I solution

---
+++ 08/src/run.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define LAYER_LENGTH = 150;
+
+int main(int argc, char* argv[]) {
+  char ch;
+  int product = -1;
+  int min = -1;
+  int count_zeros = 0;
+  int count_ones = 0;
+  int count_twos = 0;
+  int pos = 1;
+  int layer = 0;
+  while((ch = getchar()) != EOF) {
+    int val = atoi(&ch);
+    if (val == 1) {
+      count_ones++;
+    } else if (val == 2) {
+      count_twos++;
+    } else if (val == 0) {
+      count_zeros++;
+    }
+
+    int current_layer = pos++ / 150;
+    if (current_layer != layer) {
+      layer = current_layer;
+      if (min == -1 || min > count_zeros) {
+        min = count_zeros;
+        product = count_ones * count_twos;
+      }
+
+      count_zeros = 0;
+      count_ones = 0;
+      count_twos = 0;
+    }
+  }
+  printf("the min product is: %d\n", product);
+  return 0;
+}
